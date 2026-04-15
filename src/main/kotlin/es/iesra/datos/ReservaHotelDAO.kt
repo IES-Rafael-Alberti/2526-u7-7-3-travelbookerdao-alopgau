56ed76286657ff2fe12e6792aae36dedd9bfc00f
alopgau <alopezg@ieslabahia.es>
2026-04-15 22:04:27 +0200

    Cambiando DAO

--- src/main/kotlin/es/iesra/datos/ReservaHotelDAO.kt
+++ src/main/kotlin/es/iesra/datos/ReservaHotelDAO.kt
@@ -4,33 +4,34 @@ import es.iesra.dominio.ReservaHotel
 import java.io.File
 
 data class ReservaHotelDAO(
-    val id: String,
-    val descripcion: String,
-    val ubicacion: String,
-    val numeroNoches: String
+    private val file: File
 ): DAO<ReservaHotel> {
-    override fun create() = File("travelbooker.csv").appendText("$id,$descripcion,$ubicacion,$numeroNoches")
 
-    override fun read(criteria: (String) -> Boolean ): ReservaHotel? {
-        val target = File("travelbooker.csv").readLines().find(criteria)
-        if (target != null) {
-        val targetSplitted = target.split(",")
-        return ReservaHotel.recuperaInstancia(targetSplitted[0].toInt(),targetSplitted[1],targetSplitted[2],targetSplitted[3].toInt())
+
+    override fun create(reserva: ReservaHotel) = file.appendText("${reserva.id},${reserva.descripcion},${reserva.ubicacion},${reserva.numeroNoches}")
+
+    override fun read(reserva: ReservaHotel ): ReservaHotel? {
+        val lines = file.readLines()
+        val linesMap = lines.map {
+            val lineSplit = it.split(",")
+        ReservaHotel.recuperaInstancia(lineSplit[0].toInt(),lineSplit[1],lineSplit[2],lineSplit[3].toInt())
         }
-        return null
-    }
+        return linesMap.find(criteria)
+
+        }
 
     override fun update(criteria: (String) -> Boolean, newDesc: String?, newUbi: String?, newNights: String?) {
-        val f = File("travelbooker.csv").readLines().toMutableList()
-        val target = f.find(criteria)
-        val targetIndex = f.indexOf(target)
+        val fileLines = file.readLines().toMutableList()
+        val target = fileLines.find(criteria)
+        val targetIndex = fileLines.indexOf(target)
         if (target != null) {
             val targetSplitted = target.split(",")
-            f[targetIndex] = "${targetSplitted[0]},${newDesc ?: targetSplitted[1]},${newUbi ?: targetSplitted[2]},${newNights ?: targetSplitted[3]}"
+            fileLines[targetIndex] = "${targetSplitted[0]},${newDesc ?: targetSplitted[1]},${newUbi ?: targetSplitted[2]},${newNights ?: targetSplitted[3]}"
 
-            f.forEach {File("travelbooker.csv").writeText(it)}
+            fileLines.forEach {file.writeText(it)}
         }
 
     }
     override fun delete(criteria: (String) -> Boolean) =  File("travelbooker.csv").readLines().filter(criteria)
 }
+    }
